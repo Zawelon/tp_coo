a810eed3b42bf499accaef8ab9235524b20ba1da
Gono G <zawelongoumou@gmail.com>
2024-09-25 11:42:06 +0200

    add class Local

--- crayon/high_level/models.py
+++ crayon/high_level/models.py
@@ -6,3 +6,6 @@ class Ville(models.Model):
     nom = models.CharField(max_length=100)
     code_postal = models.IntegerField()
     prix_par_m2 = models.IntegerField()
+
+    def __str__(self):
+        return self.nom
