a9c0fac96a68776ad67e8123d63e72ebce8c82ab
gmg1244a <gmg1244a@u2-211-06d.univ-tlse3.fr>
2024-10-04 14:46:20 +0200

    creation de la classe Ville en C++

--- crayon/low_level/low_level.cpp
+++ crayon/low_level/low_level.cpp
@@ -1,28 +1,39 @@
-#include "iostream"
+#include <iostream>
+#include <string>
 
-class Departement() {
-  std::string nom;
-  int code_postal;
-  float pris_par_m2;
+// Classe Ville avec les attributs nom, code_postal, et pris_par_m2
+class Ville {
+private:
+    std::string nom;      // Le nom de la ville
+    int code_postal;       // La code_postal de la ville
+    double pris_par_m2;    // La pris_par_m2 de la ville en km²
 
-  // Constructeur  sans parametre
-  Departement() {
-    nom = "Non defini";
-    code_postal = 0;
-    pris_par_m2 = 0;
-  }
+public:
+    // Constructeur prenant directement les attributs
+    Ville(std::string vill_name, int vill_code_postal, double ville_price) : nom{vill_name}, code_postal{vill_code_postal}, pris_par_m2{ville_price} {}
 
-  // Constructeur  avec parametres
-  Departement(std::string nom, int code_postal, int pris_par_m2) {
-    this->nom = nom;
-    this->code_postal = code_postal;
-    this->pris_par_m2 = pris_par_m2;
-  }
+    // Méthode d'affichage pour afficher les détails de la ville
 
-  // Methode d'affichage
-  void Affichage() {
-    std::cout << "Le nom du departement est: " << nom << std::endl;
-    std::cout << "Le code postal est: " << code_postal << std::endl;
-    std::cout << "Le le prix par metre carre est: " << pris_par_m2 << std::endl;
-  }
+    void afficher() const {
+        std::cout << "Ville: " << nom << std::endl;
+        std::cout << "code_postal: " << code_postal << std::endl;
+        std::cout << "pris_par_m2: " << pris_par_m2 << " km²" << std::endl;
+    }
+
+    // Fonction amie, surcharge de l'opérateur <<
+    friend std::ostream& operator<<(std::ostream& out, const Ville& ville) {
+        return out << "Nom: " << ville.nom << " | code_postal: " << ville.code_postal
+                   << " | pris_par_m2: " << ville.pris_par_m2 << " Euros/km²";
+    }
+};
+
+// Fonction principale qui crée une instance de Ville et l'affiche
+auto main() -> int {
+    // Créer une instance de Ville avec des attributs spécifiques
+    Ville ville("Toulouse", 471941, 118.3);
+
+    // Appeler la méthode afficher pour afficher les détails de la ville
+    ville.afficher();
+
+    return 0;
 }
