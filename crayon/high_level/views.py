841fc5197fc5a7377e286047fa45fedeeda0f42b
Zawelon <zawelongoumou@gmail.com>
2024-11-27 11:54:36 +0100

    Update views.py

--- crayon/high_level/views.py
+++ crayon/high_level/views.py
@@ -1,6 +1,8 @@
+#Creation de la View
 from django.views.generic import DetailView
 from django.http import JsonResponse
-from .models import Ville, Usine, Produit
+from .models import * #Importation de toutes les classe crees dans le fichier models.py
+from django.shortcuts import render, get_object_or_404
 
 
 class VilleJsonDetailView(DetailView):
@@ -24,15 +26,10 @@ class ProduitJsonDetailView(DetailView):
         return JsonResponse(self.object.json(), **response_kwargs)
 
 
-from django.shortcuts import render, get_object_or_404
-
 def produire_produit(request, produit_id, quantite):
     usine = Usine.objects.first()  
     produit = get_object_or_404(Produit, id=produit_id)
-
-
     usine.approvisionnement_automatique(produit, quantite)
-
 
     return render(request, 'production.html', {
         'usine': usine,
