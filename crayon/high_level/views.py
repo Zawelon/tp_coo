34ece4395ab4ff36d0bfbfb64d8a2cb9a3432eaf
Zawelon <zawelongoumou@gmail.com>
2024-12-05 20:58:05 +0100

    Update views.py
    Ajout de UsineApiView dans  viens.py

--- crayon/high_level/views.py
+++ crayon/high_level/views.py
@@ -37,6 +37,13 @@ class UsineJsonDetailView(DetailView):
     ) -> HTTPR:
         return JsonResponse(self.object.json())
 
+class UsineApiView(DetailView):
+    model = Usine
+
+    def render_to_response(
+        self, context: dict[str, Any], **response_kwargs: Any
+    ) -> HTTPR:
+        return JsonResponse(self.object.json_extended())
 
 class ProduitJsonDetailView(DetailView):
     model = Produit
@@ -117,77 +124,3 @@ class EtapeJsonDetailView(DetailView):
         self, context: dict[str, Any], **response_kwargs: Any
     ) -> HTTPR:
         return JsonResponse(self.object.json())
-
-
-"""
-# Partie ApiView
-from django.shortcuts import get_object_or_404
-from rest_framework.views import APIView
-from rest_framework.response import Response
-
-# API Views (héritage de DRF)
-class VilleApiView(APIView):
-    def get(self, request, pk):
-        ville = get_object_or_404(Ville, pk=pk)
-        return Response(ville.json())
-
-
-class UsineApiView(APIView):
-    def get(self, request, pk):
-        usine = get_object_or_404(Usine, pk=pk)
-        return Response(usine.json())
-
-
-class ProduitApiView(APIView):
-    def get(self, request, pk):
-        produit = get_object_or_404(Produit, pk=pk)
-        return Response(produit.json())
-
-
-class LocalApiView(APIView):
-    def get(self, request, pk):
-        local = get_object_or_404(Local, pk=pk)
-        return Response(local.json())
-
-
-class SiegeSocialApiView(APIView):
-    def get(self, request, pk):
-        siege_social = get_object_or_404(SiegeSocial, pk=pk)
-        return Response(siege_social.json())
-
-
-class MachineApiView(APIView):
-    def get(self, request, pk):
-        machine = get_object_or_404(Machine, pk=pk)
-        return Response(machine.json())
-
-
-class ObjetApiView(APIView):
-    def get(self, request, pk):
-        objet = get_object_or_404(Objet, pk=pk)
-        return Response(objet.json())
-
-
-class StockApiView(APIView):
-    def get(self, request, pk):
-        stock = get_object_or_404(Stock, pk=pk)
-        return Response(stock.json())
-
-
-class RessourceApiView(APIView):
-    def get(self, request, pk):
-        ressource = get_object_or_404(Ressource, pk=pk)
-        return Response(ressource.json())
-
-
-class QuantiteRessourceApiView(APIView):
-    def get(self, request, pk):
-        quantite_ressource = get_object_or_404(QuantiteRessource, pk=pk)
-        return Response(quantite_ressource.json())
-
-
-class EtapeApiView(APIView):
-    def get(self, request, pk):
-        etape = get_object_or_404(Etape, pk=pk)
-        return Response(etape.json())
-"""
